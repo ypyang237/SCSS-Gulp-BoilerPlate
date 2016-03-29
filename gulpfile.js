9dcba490a6ce2e336ab7b2fd3260966f00033e21
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-28 15:43:13 -1000

    adds gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
-'use strict';
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
-});       //
+  browserSync.init({
+      server: "./public"
+  });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch("scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/styles.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
